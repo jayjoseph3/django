0c464003bebc22631de1fb475951cfc5d1c4e3e9
jayjoseph3 <root@centos7.localdomain>
2017-07-05 17:35:45 +0800

    admin.py in howdy

---
+++ helloapp/howdy/admin.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from howdy.models import Dreamreal
+
+admin.site.register(Dreamreal)
+
+# Register your models here.
